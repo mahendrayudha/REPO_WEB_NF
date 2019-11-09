a6ac6eff2be47a5a9e504dd3d73cdcace4213559
mahendrayudha <yudhaoctavian01@gmail.com>
2019-11-09 08:35:21 +0700

    Update beranda.php

--- WEB_NF_2/beranda.php
+++ WEB_NF_2/beranda.php
@@ -8,12 +8,7 @@
     <title>Naura Farm</title>
     <link href="http://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet" />
     <link rel="shortcut icon" href="assets/images/ic_nf.ico" />
-    <link href="assets/style.css" rel="stylesheet" type="text/css" media="all" />
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link href="style.css" rel="stylesheet" type="text/css" media="all" />
 
 </head>
 
@@ -102,36 +97,18 @@
                 <p>Informasi seputar buah naga yang kami produksi</p>
             </div>
 
-            <div class="card" style="width:400px">
-                <img class="card-img-top" src="assets/images/RED.png" alt="Card image" style="width:100%">
-                <div class="card-body">
-                  <h4 class="card-title">Buah Naga Merah</h4>
-                  <p class="card-text">Hylocereus polyrhizus</p>
-                  <a href="#" class="btn btn-primary">Info Lengkap</a>
-                </div>
-            </div>
-
-            <div class="card" style="width:400px">
-                <img class="card-img-top" src="assets/images/WHITE.png" alt="Card image" style="width:100%">
-                <div class="card-body">
-                  <h4 class="card-title">Buah Naga Putih</h4>
-                  <p class="card-text">Hylocereus undatus</p>
-                  <a href="#" class="btn btn-primary">Info Lengkap</a>
-                </div>
-            </div>
-
-            <!-- <div class="box-red">
+            <div class="box-red">
                 <img src="assets/images/RED.png" alt="Buah Naga Merah">
                 <h3>Buah Naga Merah</h3>
                 <p>Hylocereus polyrhizus</p>
                 <a href="produk.html" class="button button-alt">More Info</a>
-            </div> -->
-            <!-- <div class="box-white">
+            </div>
+            <div class="box-white">
                 <img src="assets/images/WHITE.png" alt="Buah Naga Putih">
                 <h3>Buah Naga Putih</h3>
                 <p>Hylocereus undatus</p>
                 <a href="produk.html" class="button button-alt">More Info</a>
-            </div> -->
+            </div>
         </div>
     </div>
 <!-------------------------- CONTENT PRODUK-BUAH ---------------------------->
