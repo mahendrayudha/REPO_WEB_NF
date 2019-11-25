c8690da6d90c191eb37b657d1cee06d38bba521e
iBOT03 <andrea.santana986@gmail.com>
2019-11-25 14:14:21 +0700

    Update homepage.php

--- WEB_NF_3/homepage.php
+++ WEB_NF_3/homepage.php
@@ -303,14 +303,21 @@
         </div>
         <div class="col-xl-4 col-lg-5">
           <div class="featured-text text-center text-lg-left">
-            <h4>Shoreline</h4>
+            <h4>Pesan Produk</h4>
             <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any
               project - even commercial projects! Download it, customize it, and publish your website!</p>
           </div>
         </div>
       </div>
 
-      <!-- Project One Row -->
+    </div>
+  </section>
+
+  <!-- fasilitas Section -->
+  <section id="produk" class="produk-section bg-light">
+    <div class="container">
+
+      <!-- Produk Jus Section -->
       <div id="produk" class="row justify-content-center no-gutters mb-5 mb-lg-0">
         <div class="col-lg-6">
           <img class="img-fluid" src="img/demo-image-01.jpg" alt="">
@@ -319,7 +326,7 @@
           <div class="bg-black text-center h-100 project">
             <div class="d-flex h-100">
               <div class="project-text w-100 my-auto text-center text-lg-left">
-                <h4 class="text-white">Misty</h4>
+                <h4 class="text-white">Jus Buah Naga</h4>
                 <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else,
                   along with a description.</p>
                 <hr class="d-none d-lg-block mb-0 ml-0">
@@ -329,7 +336,7 @@
         </div>
       </div>
 
-      <!-- Project Two Row -->
+      <!-- Produk Keripik Section -->
       <div class="row justify-content-center no-gutters">
         <div class="col-lg-6">
           <img class="img-fluid" src="img/demo-image-02.jpg" alt="">
@@ -338,7 +345,7 @@
           <div class="bg-black text-center h-100 project">
             <div class="d-flex h-100">
               <div class="project-text w-100 my-auto text-center text-lg-right">
-                <h4 class="text-white">Mountains</h4>
+                <h4 class="text-white">Keripik Buah Naga</h4>
                 <p class="mb-0 text-white-50">Another example of a project with its respective description. These
                   sections work well responsively as well, try this theme on a small screen!</p>
                 <hr class="d-none d-lg-block mb-0 mr-0">
