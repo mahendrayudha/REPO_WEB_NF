ec47e36ebfa133e652b37b26014cb74c4dd63fe6
mahendrayudha <yudhaoctavian01@gmail.com>
2019-11-25 14:15:33 +0700

    dfbg

--- WEB_NF_3/homepage.php
+++ WEB_NF_3/homepage.php
@@ -292,14 +292,14 @@
     </div>
   </section>
 
-  <!-- fasilitas Section -->
+  <!-- Fasilitas Section -->
   <section id="fasilitas" class="produk-section bg-light">
     <div class="container">
 
       <!-- Featured Project Row -->
       <div class="row align-items-center no-gutters mb-4 mb-lg-5">
         <div class="col-xl-8 col-lg-7">
-          <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
+          <img class="img-fluid mb-3 mb-lg-0" src="img/Agrowisata.png" alt="">
         </div>
         <div class="col-xl-4 col-lg-5">
           <div class="featured-text text-center text-lg-left">
@@ -310,16 +310,16 @@
         </div>
       </div>
 
-      <!-- Project One Row -->
+      <!-- Produk Buah Merah -->
       <div id="produk" class="row justify-content-center no-gutters mb-5 mb-lg-0">
         <div class="col-lg-6">
-          <img class="img-fluid" src="img/demo-image-01.jpg" alt="">
+          <img class="img-fluid" src="img/buah_merah.jpg" alt="">
         </div>
         <div class="col-lg-6">
           <div class="bg-black text-center h-100 project">
             <div class="d-flex h-100">
               <div class="project-text w-100 my-auto text-center text-lg-left">
-                <h4 class="text-white">Misty</h4>
+                <h4 class="text-white">Buah Naga Merah</h4>
                 <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else,
                   along with a description.</p>
                 <hr class="d-none d-lg-block mb-0 ml-0">
@@ -329,16 +329,16 @@
         </div>
       </div>
 
-      <!-- Project Two Row -->
+      <!-- Produk Buah Putih -->
       <div class="row justify-content-center no-gutters">
         <div class="col-lg-6">
-          <img class="img-fluid" src="img/demo-image-02.jpg" alt="">
+          <img class="img-fluid" src="img/buah_putih.jpg" alt="">
         </div>
         <div class="col-lg-6 order-lg-first">
           <div class="bg-black text-center h-100 project">
             <div class="d-flex h-100">
               <div class="project-text w-100 my-auto text-center text-lg-right">
-                <h4 class="text-white">Mountains</h4>
+                <h4 class="text-white">Buah Naga Putih</h4>
                 <p class="mb-0 text-white-50">Another example of a project with its respective description. These
                   sections work well responsively as well, try this theme on a small screen!</p>
                 <hr class="d-none d-lg-block mb-0 mr-0">
@@ -347,7 +347,6 @@
           </div>
         </div>
       </div>
-
     </div>
   </section>
 
