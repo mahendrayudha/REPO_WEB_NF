f30849bdb249870c4d4479554ff9adf588267223
iBOT03 <andrea.santana986@gmail.com>
2019-11-26 16:39:28 +0700

    Update homepage.php

--- WEB_NF_3/homepage.php
+++ WEB_NF_3/homepage.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php session_start();
-require 'functions.php';?>
+require 'functions.php'; ?>
+
 <head>
 
   <meta charset="utf-8">
@@ -13,9 +14,7 @@ require 'functions.php';?>
   <!-- Custom fonts -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles -->
   <link href="css/style.css" rel="stylesheet">
@@ -35,9 +34,7 @@ require 'functions.php';?>
           Naura Farm
         </a>
       </div>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-        data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-        aria-label="Toggle navigation">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
       </button>
@@ -105,14 +102,7 @@ require 'functions.php';?>
           }
         </script>
 
-        <button type="submit" name="masuk" onclick="hidden()">Masuk</button>
-        <!-- <script>
-          function hidden {
-            if (masuk.onclick) {
-              
-            }
-          }
-        </script> -->
+        <button type="submit" name="masuk">Masuk</button>
         <label>
           <input type="checkbox" checked="checked" name="remember" />
           Remember me
@@ -131,90 +121,89 @@ require 'functions.php';?>
         <script>
           function hide() {
             document.getElementById('id01').style.display = 'none';
-            document.getElementById('id02').style.display='block';
+            document.getElementById('id02').style.display = 'block';
           }
         </script>
       </div>
     </form>
   </div>
 
-<!-- action="/action_page.php"  -->
+  <!-- action="/action_page.php"  -->
 
   <div id="id02" class="modal">
-      <form class="modal-content animate" method="post">
-          <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Tutup">
-              &times;
-          </span>
-          <div class="imgcontainer">
-              <img src="img/user.svg" alt="Avatar" class="avatar" />
-          </div>
+    <form class="modal-content animate" method="post">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Tutup">
+        &times;
+      </span>
+      <div class="imgcontainer">
+        <img src="img/user.svg" alt="Avatar" class="avatar" />
+      </div>
 
-          <div class="container">
-              <input type="hidden" name="ID_USER" value="" readonly />
-              <input type="hidden" name="ID_STATUS" value="02" readonly />
-              
-              <label for="nama"><b>Nama</b></label>
-              <input type="text" placeholder="Enter Name" name="nama" required />
+      <div class="container">
+        <input type="hidden" name="ID_USER" value="" readonly />
+        <input type="hidden" name="ID_STATUS" value="02" readonly />
 
-              <div class="text"></div><label for="notelp"><b>Nomor Telpon</b></label>
-              <input type="text" placeholder="Enter Nomor Telpon" name="notelp" required />
+        <label for="nama"><b>Nama</b></label>
+        <input type="text" placeholder="Enter Name" name="nama" required />
 
-              <div class="text1"></div><label for="alamat"><b>Alamat</b></label>
-              <input type="text" placeholder="Enter Alamat" name="alamat" required />
+        <div class="text"></div><label for="notelp"><b>Nomor Telpon</b></label>
+        <input type="text" placeholder="Enter Nomor Telpon" name="notelp" required />
 
-              <div class="text2"></div><label for="email"><b>Email</b></label>
-              <input type="text" placeholder="Enter Email" name="email" required />
+        <div class="text1"></div><label for="alamat"><b>Alamat</b></label>
+        <input type="text" placeholder="Enter Alamat" name="alamat" required />
 
-              <div class="text3"></div><label for="username"><b>Username</b></label>
-              <input type="text" placeholder="Enter Username" name="username" required />
+        <div class="text2"></div><label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required />
 
-              <div class="text4"></div><label for="psw"><b>Password</b></label>
-              <input type="password" placeholder="Enter Password" name="psw" value="" id="myInput" required />
-              <div class="text5"></div><input type="checkbox" onclick="myFunction()">
-              <div class="show">Show Password</div>
+        <div class="text3"></div><label for="username"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="username" required />
 
-              <script>
-                  function myFunction() {
-                      var x = document.getElementById("myInput");
-                      if (x.type === "password") {
-                          x.type = "text";
-                      } else {
-                          x.type = "password";
-                      }
-                  }
-              </script>
+        <div class="text4"></div><label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" value="" id="myInput" required />
+        <div class="text5"></div><input type="checkbox" onclick="myFunction()">
+        <div class="show">Show Password</div>
 
-              <button type="submit" name="daftar">Daftar</button>
-              <label>
-                  <div class="text6"></div><input type="checkbox" checked="checked" name="remember" />
-                  Remember me
-              </label>
-          </div>
-          <div class="container" style="background-color:#f1f1f1">
-              <!-- <button
+        <script>
+          function myFunction() {
+            var x = document.getElementById("myInput");
+            if (x.type === "password") {
+              x.type = "text";
+            } else {
+              x.type = "password";
+            }
+          }
+        </script>
+
+        <button type="submit" name="daftar">Daftar</button>
+        <label>
+          <div class="text6"></div><input type="checkbox" checked="checked" name="remember" />
+          Remember me
+        </label>
+      </div>
+      <div class="container" style="background-color:#f1f1f1">
+        <!-- <button
         type="button"
         onclick="document.getElementById('id01').style.display='none'"
         class="cancelbtn">
         Cancel
       </button> -->
-              <span class="psw">Sudah Punya Akun? <a href="" onclick="back() name="daftar" ">Masuk</a></span>
-              <script>
+        <span class="psw">Sudah Punya Akun? <a href="" onclick="back()" name="daftar">Masuk</a></span>
+        <script>
           function back() {
             document.getElementById('id02').style.display = 'none';
-            document.getElementById('id01').style.display='block';
+            document.getElementById('id01').style.display = 'block';
           }
         </script>
-          </div>
-      </form>
+      </div>
+    </form>
   </div>
 
   <script>
-
     // Get the modal
     var modal = document.getElementById("id01");
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
+    window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
       }
@@ -222,64 +211,55 @@ require 'functions.php';?>
   </script>
 
   <!-- /*/////////////////////////////////// CONNECTION DATABASE //////////////////////////////////*/ -->
-  <!-- ////////////////////////////////////////DATABASE LOGIN///////////////////////////////////////// -->
+  <!-- //////////////////////////////////////  DATABASE LOGIN  /////////////////////////////////////// -->
   <?php
-  if(isset($_POST["masuk"])){
+  if (isset($_POST["masuk"])) {
 
-  $username = $_POST['nama'];
-  $password = $_POST['psw'];
+    $username = $_POST['nama'];
+    $password = $_POST['psw'];
 
 
-  $result = mysqli_query($conn, "SELECT * FROM user WHERE USERNAME = '$username' AND PASSWORD = '$password'");
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE USERNAME = '$username' AND PASSWORD = '$password'");
 
-  if( mysqli_num_rows($result) === 1 )
-  {
-    //cek password
-    $row = mysqli_fetch_assoc($result);
-    $_SESSION["login"] = true;
-    $_SESSION['user'] = $row ["NAMA"];
-    // header("location: homepage.php");
-    echo "<script>alert ('Login Berhasil')</script>";
+    if (mysqli_num_rows($result) === 1) {
+      //cek password
+      $row = mysqli_fetch_assoc($result);
+      $_SESSION["login"] = true;
+      $_SESSION['user'] = $row["NAMA"];
+      // header("location: homepage.php");
+      echo "<script>alert ('Login Berhasil')</script>";
+    } else {
+      echo "<script>alert ('Login Gagal')</script>";
+      // header("location: login.php?gagal");
+    }
   }
-  else
-  {
-    echo "<script>alert ('Login Gagal')</script>";
-    // header("location: login.php?gagal");
-  }
-}
-?>
+  ?>
 
- <!-- ////////////////////////////////////////DATABASE REGISTER////////////////////////////////////////// -->
- <?php
+  <!-- ////////////////////////////////////////DATABASE REGISTER////////////////////////////////////////// -->
+  <?php
 
-//cek tombol sudah ditekan atau belum
-if( isset ($_POST["daftar"]) )
-{
- 
-        //cek data berhasil ditambah?
-        if( tambah($_POST) > 0 )
-        {
-                echo "<script>
+  //cek tombol sudah ditekan atau belum
+  if (isset($_POST["daftar"])) {
+
+    //cek data berhasil ditambah?
+    if (tambah($_POST) > 0) {
+      echo "<script>
                 alert('Data Berhasil Ditambahkan');
                 document.location.href = 'homepage.php';
                 </script>";
-        }
-        else
-        {
-                echo "<script>alert('Gagal Menambahkan Data')</script>";
-        }
-       
-}
-if(isset($_POST["batal"]))
-{
-        header("Location: homepage.php");
-        exit;
-}
-?>
+    } else {
+      echo "<script>alert('Gagal Menambahkan Data')</script>";
+    }
+  }
+  if (isset($_POST["batal"])) {
+    header("Location: homepage.php");
+    exit;
+  }
+  ?>
 
 
 
-<!-- ////////////////////////////Profil Section/////////////////////////// -->
+  <!-- ////////////////////////////Profil Section/////////////////////////// -->
   <section id="profil" class="profil-section text-center">
     <div class="container">
       <div class="row">
@@ -296,11 +276,11 @@ if(isset($_POST["batal"]))
     </div>
   </section>
 
-<!-- ////////////////////////////Produk Section/////////////////////////// -->
+  <!-- ////////////////////////////Produk Section/////////////////////////// -->
   <section id="produk" class="produk-section bg-light">
     <div class="container">
 
-  <!-- /////////////////////Produk Buah Naga Merah//////////////////////// -->
+      <!-- /////////////////////Produk Buah Naga Merah//////////////////////// -->
       <div id="produk" class="row justify-content-center no-gutters mb-5 mb-lg-0">
         <div class="col-lg-6">
 
@@ -321,7 +301,7 @@ if(isset($_POST["batal"]))
         </div>
       </div>
 
-  <!-- ///////////////////////Produk Buah Naga Putih///////////////////// -->
+      <!-- ///////////////////////Produk Buah Naga Putih///////////////////// -->
       <div class="row justify-content-center no-gutters">
         <div class="col-lg-6">
           <img class="img-fluid" src="img/buah_putih.jpg" alt="">
@@ -343,11 +323,11 @@ if(isset($_POST["batal"]))
     </div>
   </section>
 
-<!-- //////////////////////////Fasilitas Section////////////////////////// -->
+  <!-- //////////////////////////Fasilitas Section////////////////////////// -->
   <section id="fasilitas" class="produk-section bg-light">
     <div class="container">
 
-  <!-- ////////////////////////Fasilitas Agrowisata/////////////////////// -->
+      <!-- ////////////////////////Fasilitas Agrowisata/////////////////////// -->
       <div class="row align-items-center no-gutters mb-4 mb-lg-5">
         <div class="col-xl-8 col-lg-7">
           <img class="img-fluid mb-3 mb-lg-0" src="img/Agrowisata.png" alt="">
@@ -359,35 +339,35 @@ if(isset($_POST["batal"]))
               project - even commercial projects! Download it, customize it, and publish your website!</p>
           </div>
         </div>
-        
-  <!-- ////////////////////////Fasilitas Petik Buah/////////////////////// -->
-  <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-        <div class="col-xl-8 col-lg-7">
-          <img class="img-fluid mb-3 mb-lg-0" src="img/petik_buah.png" alt="">
-        </div>
-        <div class="col-xl-4 col-lg-5">
-          <div class="featured-text text-center text-lg-left">
-            <h4>Petik Buah</h4>
-            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any
-              project - even commercial projects! Download it, customize it, and publish your website!</p>
+
+        <!-- ////////////////////////Fasilitas Petik Buah/////////////////////// -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+          <div class="col-xl-8 col-lg-7">
+            <img class="img-fluid mb-3 mb-lg-0" src="img/petik_buah.png" alt="">
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="featured-text text-center text-lg-left">
+              <h4>Petik Buah</h4>
+              <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any
+                project - even commercial projects! Download it, customize it, and publish your website!</p>
+            </div>
+          </div>
+
+          <!-- ////////////////////////Fasilitas Penjualan Produk/////////////////////// -->
+          <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+            <div class="col-xl-8 col-lg-7">
+              <img class="img-fluid mb-3 mb-lg-0" src="img/penjualan_produk.png" alt="">
+            </div>
+            <div class="col-xl-4 col-lg-5">
+              <div class="featured-text text-center text-lg-left">
+                <h4>Penjualan Produk</h4>
+                <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any
+                  project - even commercial projects! Download it, customize it, and publish your website!</p>
+              </div>
+            </div>
+
           </div>
         </div>
-
-        <!-- ////////////////////////Fasilitas Penjualan Produk/////////////////////// -->
-      <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-        <div class="col-xl-8 col-lg-7">
-          <img class="img-fluid mb-3 mb-lg-0" src="img/penjualan_produk.png" alt="">
-        </div>
-        <div class="col-xl-4 col-lg-5">
-          <div class="featured-text text-center text-lg-left">
-            <h4>Penjualan Produk</h4>
-            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any
-              project - even commercial projects! Download it, customize it, and publish your website!</p>
-          </div>
-        </div>
-
-      </div>
-    </div>
   </section>
 
   <!-- Contact Section -->
@@ -396,27 +376,27 @@ if(isset($_POST["batal"]))
       <div class="row">
         <div class="col-md-10 col-lg-8 mx-auto text-center">
 
-        <table class="kontak_kami_content"; style="padding-left: 100px; padding-right: 100px">
-        <tr>
-          <td>Alamat</td>
-          <td>: Jl. Tidar No.27, Kloncing Sumbersari, Kab. Jember, Jawa Timur 68124</td>
-        </tr>
+          <table class="kontak_kami_content" ; style="padding-left: 100px; padding-right: 100px">
+            <tr>
+              <td>Alamat</td>
+              <td>: Jl. Tidar No.27, Kloncing Sumbersari, Kab. Jember, Jawa Timur 68124</td>
+            </tr>
 
-        <tr>
-          <td>Telepon</td>
-          <td>: 085229728848</td>
-        </tr>
+            <tr>
+              <td>Telepon</td>
+              <td>: 085229728848</td>
+            </tr>
 
-        <tr>
-          <td>Whatsapp</td>
-          <td>: 085229728848</td>
-        </tr>
+            <tr>
+              <td>Whatsapp</td>
+              <td>: 085229728848</td>
+            </tr>
 
-        <tr>
-          <td>Instagram</td>
-          <td>: @naurafarm_jember</td>
-        </tr>
-      </table>
+            <tr>
+              <td>Instagram</td>
+              <td>: @naurafarm_jember</td>
+            </tr>
+          </table>
 
         </div>
       </div>
