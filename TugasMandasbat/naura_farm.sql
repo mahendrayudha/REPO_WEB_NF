00903ac8f449d489e3420ad3bfe0a76ff424bff6
iBOT03 <andrea.santana986@gmail.com>
2019-12-03 12:08:55 +0700

    Update naura_farm.sql

--- TugasMandasbat/naura_farm.sql
+++ TugasMandasbat/naura_farm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Nov 2019 pada 02.38
+-- Waktu pembuatan: 03 Des 2019 pada 06.07
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 5.6.40
 
@@ -25,68 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `opsi_pembayaran`
+-- Struktur dari tabel `detail_jual`
 --
 
-CREATE TABLE `opsi_pembayaran` (
-  `ID_OPSI_PEMBAYARAN` varchar(3) NOT NULL,
-  `PEMBAYARAN` varchar(10) DEFAULT NULL
+CREATE TABLE `detail_jual` (
+  `ID_DETAIL` int(5) NOT NULL,
+  `ID_TRANSAKSI` int(5) NOT NULL,
+  `ID_PRODUK` int(5) NOT NULL,
+  `JUMLAH_BELI` int(3) NOT NULL,
+  `SUB_TOTAL` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `opsi_pengiriman`
+-- Struktur dari tabel `produk`
 --
 
-CREATE TABLE `opsi_pengiriman` (
-  `ID_OPSI_PENGIRIMAN` varchar(3) NOT NULL,
-  `PENGIRIMAN` varchar(10) DEFAULT NULL
+CREATE TABLE `produk` (
+  `ID_PRODUK` varchar(3) NOT NULL,
+  `NAMA_PRODUK` varchar(30) DEFAULT NULL,
+  `STOK_PRODUK` int(11) DEFAULT NULL,
+  `HARGA_BELI` int(11) NOT NULL,
+  `HARGA_JUAL` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `produk_buah`
---
-
-CREATE TABLE `produk_buah` (
-  `ID_PRODUK_BUAH` varchar(3) NOT NULL,
-  `NAMA_PRODUK_BUAH` varchar(30) DEFAULT NULL,
-  `STOK_PRODUK_BUAH` int(11) DEFAULT NULL,
-  `HARGA_PRODUK_BUAH` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `produk_olahan`
---
-
-CREATE TABLE `produk_olahan` (
-  `ID_PRODUK_OLAHAN` varchar(3) NOT NULL,
-  `NAMA_PRODUK_OLAHAN` varchar(30) DEFAULT NULL,
-  `STOK_PRODUK_OLAHAN` int(11) DEFAULT NULL,
-  `HARGA_PRODUK_OLAHAN` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `status`
---
-
-CREATE TABLE `status` (
-  `ID_STATUS` int(2) NOT NULL,
-  `STATUS` varchar(5) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `status`
---
-
-INSERT INTO `status` (`ID_STATUS`, `STATUS`) VALUES
-(1, 'ADMIN');
 
 -- --------------------------------------------------------
 
@@ -96,15 +58,13 @@ INSERT INTO `status` (`ID_STATUS`, `STATUS`) VALUES
 
 CREATE TABLE `transaksi` (
   `ID_TRANSAKSI` varchar(5) NOT NULL,
-  `ID_OPSI_PEMBAYARAN` varchar(3) DEFAULT NULL,
-  `ID_OPSI_PENGIRIMAN` varchar(3) DEFAULT NULL,
-  `ID_USER` varchar(5) DEFAULT NULL,
-  `ID_PRODUK_BUAH` varchar(3) DEFAULT NULL,
-  `ID_PRODUK_OLAHAN` varchar(3) DEFAULT NULL,
+  `ID_USER` int(5) NOT NULL,
   `TANGGAL_TRANSAKSI` datetime DEFAULT NULL,
-  `JUMLAH_PRODUK` int(11) DEFAULT NULL,
+  `STATUS_BAYAR` int(3) DEFAULT NULL COMMENT '1. Sudah Bayar, 2. Belum Bayar',
   `GRAND_TOTAL` int(11) DEFAULT NULL,
-  `ALAMAT_PENGIRIMAN` varchar(100) DEFAULT NULL
+  `ALAMAT_PENGIRIMAN` varchar(100) DEFAULT NULL,
+  `OPSI_PEMBAYARAN` int(1) NOT NULL COMMENT '1. Transfer, 2. Cash',
+  `TUJUAN_PEMBAYARAN` varchar(50) NOT NULL COMMENT 'Aun bank dll'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -114,104 +74,83 @@ CREATE TABLE `transaksi` (
 --
 
 CREATE TABLE `user` (
-  `ID_USER` varchar(5) NOT NULL,
-  `ID_STATUS` int(2) DEFAULT NULL,
+  `ID_USER` int(5) NOT NULL,
+  `ID_STATUS` int(2) NOT NULL DEFAULT '2',
   `NAMA` varchar(100) DEFAULT NULL,
   `NOMOR_TELEPON` varchar(13) DEFAULT NULL,
   `ALAMAT` varchar(150) DEFAULT NULL,
   `EMAIL` varchar(50) DEFAULT NULL,
   `USERNAME` varchar(20) DEFAULT NULL,
-  `PASSWORD` varchar(20) DEFAULT NULL
+  `PASSWORD` varchar(20) DEFAULT NULL,
+  `LEVEL` int(3) NOT NULL COMMENT '1. Admin, 2. Karyawan, 3. User'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`ID_USER`, `ID_STATUS`, `NAMA`, `NOMOR_TELEPON`, `ALAMAT`, `EMAIL`, `USERNAME`, `PASSWORD`) VALUES
-('U001', 1, 'andrea', '123456789098', 'Mastrip', 'ajonathansterben@gmail.com', 'andre', '1234');
+INSERT INTO `user` (`ID_USER`, `ID_STATUS`, `NAMA`, `NOMOR_TELEPON`, `ALAMAT`, `EMAIL`, `USERNAME`, `PASSWORD`, `LEVEL`) VALUES
+(5, 1, 'yudha', '123456789012', 'Patrang', 'yudhamahendra@gmail.com', 'yudha', 'mahendrayudha', 0),
+(6, 1, 'andrea', '098765432123', 'mastrip', 'andrea.santana986@gmail.com', 'andre', 'qwerty123', 0),
+(7, 2, 'dicky', '1234555554321', 'tidar', 'dicky@gmail.com', 'dicky', '1234', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `opsi_pembayaran`
+-- Indeks untuk tabel `detail_jual`
 --
-ALTER TABLE `opsi_pembayaran`
-  ADD PRIMARY KEY (`ID_OPSI_PEMBAYARAN`);
+ALTER TABLE `detail_jual`
+  ADD PRIMARY KEY (`ID_DETAIL`),
+  ADD UNIQUE KEY `ID_TRANSAKSI` (`ID_TRANSAKSI`),
+  ADD UNIQUE KEY `ID_PRODUK` (`ID_PRODUK`);
 
 --
--- Indeks untuk tabel `opsi_pengiriman`
+-- Indeks untuk tabel `produk`
 --
-ALTER TABLE `opsi_pengiriman`
-  ADD PRIMARY KEY (`ID_OPSI_PENGIRIMAN`);
-
---
--- Indeks untuk tabel `produk_buah`
---
-ALTER TABLE `produk_buah`
-  ADD PRIMARY KEY (`ID_PRODUK_BUAH`);
-
---
--- Indeks untuk tabel `produk_olahan`
---
-ALTER TABLE `produk_olahan`
-  ADD PRIMARY KEY (`ID_PRODUK_OLAHAN`);
-
---
--- Indeks untuk tabel `status`
---
-ALTER TABLE `status`
-  ADD PRIMARY KEY (`ID_STATUS`);
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`ID_PRODUK`);
 
 --
 -- Indeks untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`ID_TRANSAKSI`),
-  ADD KEY `FK_MEMBAYAR` (`ID_OPSI_PEMBAYARAN`),
-  ADD KEY `FK_MEMBUAT` (`ID_USER`),
-  ADD KEY `FK_MENGIRIM` (`ID_OPSI_PENGIRIMAN`),
-  ADD KEY `FK_REFERENCE_7` (`ID_PRODUK_BUAH`),
-  ADD KEY `FK_REFERENCE_8` (`ID_PRODUK_OLAHAN`);
+  ADD KEY `ID_USER` (`ID_USER`);
 
 --
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`ID_USER`),
-  ADD KEY `FK_MEMILIKI` (`ID_STATUS`);
+  ADD KEY `ID_STATUS` (`ID_STATUS`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT untuk tabel `status`
+-- AUTO_INCREMENT untuk tabel `detail_jual`
 --
-ALTER TABLE `status`
-  MODIFY `ID_STATUS` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `detail_jual`
+  MODIFY `ID_DETAIL` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID_USER` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `status`
---
-ALTER TABLE `status`
-  ADD CONSTRAINT `status_ibfk_1` FOREIGN KEY (`ID_STATUS`) REFERENCES `user` (`ID_STATUS`);
-
---
 -- Ketidakleluasaan untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD CONSTRAINT `FK_MEMBAYAR` FOREIGN KEY (`ID_OPSI_PEMBAYARAN`) REFERENCES `opsi_pembayaran` (`ID_OPSI_PEMBAYARAN`),
-  ADD CONSTRAINT `FK_MEMBUAT` FOREIGN KEY (`ID_USER`) REFERENCES `user` (`ID_USER`),
-  ADD CONSTRAINT `FK_MENGIRIM` FOREIGN KEY (`ID_OPSI_PENGIRIMAN`) REFERENCES `opsi_pengiriman` (`ID_OPSI_PENGIRIMAN`),
-  ADD CONSTRAINT `FK_REFERENCE_7` FOREIGN KEY (`ID_PRODUK_BUAH`) REFERENCES `produk_buah` (`ID_PRODUK_BUAH`),
-  ADD CONSTRAINT `FK_REFERENCE_8` FOREIGN KEY (`ID_PRODUK_OLAHAN`) REFERENCES `produk_olahan` (`ID_PRODUK_OLAHAN`);
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`ID_USER`) REFERENCES `user` (`ID_USER`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
