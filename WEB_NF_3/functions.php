6d372bd929247a1e29f44ee37f2f79139ddc128f
iBOT03 <andrea.santana986@gmail.com>
2019-12-08 15:00:38 +0700

    Update functions.php

--- WEB_NF_3/functions.php
+++ WEB_NF_3/functions.php
@@ -19,12 +19,13 @@ function tambah($data)
 {
     global $conn;
         // $id_user = htmlspecialchars($data["ID_ANGGOTA"]);
-        $nama = htmlspecialchars($data['nama']);
-        $notelp = htmlspecialchars($data['notelp']);
-        $alamat = htmlspecialchars($data['alamat']);
-        $email = htmlspecialchars($data['email']);
-        $username = htmlspecialchars($data['username']);
-        $password = htmlspecialchars($data['psw']);
+        $nama = htmlspecialchars($data['nama']) ? $_POST['nama'] : null;
+        $notelp = htmlspecialchars($data['notelp']) ? $_POST['notelp'] : null;
+        $alamat = htmlspecialchars($data['alamat']) ? $_POST['alamat'] : null;
+        $email = htmlspecialchars($data['email']) ? $_POST['email'] : null; 
+        $username = htmlspecialchars($data['username']) ? $_POST['username'] : null;
+        $password = htmlspecialchars($data['psw']) ? $_POST['psw'] : null;
+        $tambahuser = isset($_POST['tambahuseralamat']) ? $_POST['tambahuser'] : null;
         //upload foto
         // $foto = upload();
         // if( !$foto )
@@ -32,37 +33,47 @@ function tambah($data)
         //     return false;
         // }
 
-        // $qu = mysqli_query($conn, "INSERT INTO user(ID_USER, ID_STATUS, NAMA, NOMOR_TELEPON, ALAMAT, EMAIL,
-        //  USERNAME, PASSWORD) VALUES ('', '01', '$nama','$notelp','$alamat','$email','$username','$password')");
-
-        $qu = mysqli_query($conn, "INSERT INTO user VALUES ('','$nama','$notelp','$alamat','$email','$username','$password', '3')");
-        //return $qu;
-        echo "INSERT INTO user VALUES ('','$nama','$notelp','$alamat','$email','$username','$password', '3'))";
+        // $qu = mysqli_query($conn, "INSERT INTO user (NAMA, NOMOR_TELEPON, ALAMAT, EMAIL, USERNAME, PASSWORD)
+        // values ('','$nama','$notelp','$alamat','$email','$username','$password')");
+        // return $qu;
+        // echo "INSERT INTO user VALUES ('','$nama','$notelp','$alamat','$email','$username','$password', '3'))";
+        if (isset($_POST['tambahuser'])) {
+            $sql = $conn->query("insert into user (NAMA, NOMOR_TELEPON, ALAMAT, EMAIL, USERNAME, PASSWORD)
+            values('','$nama','$notelp','$alamat','$email','$username','$password')");
+            if ($sql) {
+              ?>
+              <script type="text/javascript">
+                alert("Data Berhasil Disimpan");
+                window.location.href = "homepage.php";
+              </script>
+          <?php
+            }
+          }
 }
 
 //tambah data stok
-function tambah_stok($data)
-{
-    global $conn;
-        // $id_user = htmlspecialchars($data["ID_ANGGOTA"]);
-        $nama = htmlspecialchars($data['nama']);
-        $notelp = htmlspecialchars($data['notelp']);
-        $alamat = htmlspecialchars($data['alamat']);
-        $email = htmlspecialchars($data['email']);
-        $username = htmlspecialchars($data['username']);
-        $password = htmlspecialchars($data['psw']);
-        //upload foto
-        // $foto = upload();
-        // if( !$foto )
-        // {
-        //     return false;
-        // }
+// function tambah_stok($data)
+// {
+//     global $conn;
+//         // $id_user = htmlspecialchars($data["ID_ANGGOTA"]);
+//         $nama = htmlspecialchars($data['nama']);
+//         $notelp = htmlspecialchars($data['notelp']);
+//         $alamat = htmlspecialchars($data['alamat']);
+//         $email = htmlspecialchars($data['email']);
+//         $username = htmlspecialchars($data['username']);
+//         $password = htmlspecialchars($data['psw']);
+//         //upload foto
+//         // $foto = upload();
+//         // if( !$foto )
+//         // {
+//         //     return false;
+//         // }
 
-        $qu = mysqli_query($conn, "INSERT INTO user(ID_USER, ID_STATUS, NAMA, NOMOR_TELEPON, ALAMAT, EMAIL,
-         USERNAME, PASSWORD) VALUES ('', '01', '$nama','$notelp','$alamat','$email','$username','$password')");
+//         $qu = mysqli_query($conn, "INSERT INTO user(ID_USER, ID_STATUS, NAMA, NOMOR_TELEPON, ALAMAT, EMAIL,
+//          USERNAME, PASSWORD) VALUES ('', '01', '$nama','$notelp','$alamat','$email','$username','$password')");
 
-        return $qu;
-}
+//         return $qu;
+// }
 
 //hapus data stok
 // function hapus_data($data)
