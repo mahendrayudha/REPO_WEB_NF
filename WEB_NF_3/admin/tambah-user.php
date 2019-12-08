dcdcf35d81694750ca57285530dffc8aac9b1b60
iBOT03 <andrea.santana986@gmail.com>
2019-12-08 15:00:29 +0700

    Update tambah-user.php

--- WEB_NF_3/admin/tambah-user.php
+++ WEB_NF_3/admin/tambah-user.php
@@ -20,98 +20,63 @@
 </head>
 
 <body>
-<div class="card-header">Tambah User</div>
-<div class="card-body">
-  <form method="POST">
-    <div class="form-group">
-      <div class="form-label-group">
-        <input type="text"
-               id="fullName"
-               class="form-control"
-               placeholder="Nama lengkap"
-               required="required"
-               name="namalengkap"
-               autofocus="autofocus">
-        <label for="fullName">Nama Lengkap</label>
-      </div>
-    </div>
-    
-    <div class="form-group">
-      <div class="form-label-group">
-        <input type="text"
-               id="phoneNumber"
-               class="form-control"
-               placeholder="Nomor telepon"
-               name="nomortelepon"
-               required="required">
-        <label for="phoneNumber">Nomor Telepon</label>
-      </div>
-    </div>
-
-    <div class="form-group">
-      <div class="form-label-group">
-        <input type="text"
-               id="address"
-               class="form-control"
-               placeholder="Alamat"
-               name="alamat"
-                required="required">
-        <label for="address">Alamat</label>
-      </div>
-    </div>
-
-    <div class="form-group">
-      <div class="form-label-group">
-        <input type="email"
-               id="inputEmail"
-               class="form-control"
-               placeholder="Email"
-               name="email"
-               required="required">
-        <label for="inputEmail">Email</label>
-      </div>
-    </div>
-
-    <div class="form-group">
-      <div class="form-label-group">
-        <input type="text"
-               id="username"
-               class="form-control"
-               placeholder="Username"
-               required="required"
-               name="username">
-        <label for="username">Username</label>
-      </div>
-    </div>
-
-    <div class="form-group">
-      <div class="form-row">
-        <div class="col-md-6">
-          <div class="form-label-group">
-            <input type="password"
-                   id="inputPassword"
-                   class="form-control"
-                   placeholder="Kata sandi"
-                   name="katasandi"
-                   required="required">
-            <label for="inputPassword">Kata Sandi</label>
-          </div>
+  <div class="card-header">Tambah User</div>
+  <div class="card-body">
+    <form method="POST">
+      <div class="form-group">
+        <div class="form-label-group">
+          <input type="text" id="fullName" class="form-control" placeholder="Nama lengkap" required="required" name="namalengkap" autofocus="autofocus">
+          <label for="fullName">Nama Lengkap</label>
         </div>
+      </div>
 
-        <div class="col-md-6">
-          <div class="form-label-group">
-            <input type="password"
-                   id="confirmPassword"
-                   class="form-control"
-                   placeholder="Konfirmasi kata sandi"
-                   required="required">
-            <label for="confirmPassword">Konfirmasi Kata Sandi</label>
+      <div class="form-group">
+        <div class="form-label-group">
+          <input type="text" id="phoneNumber" class="form-control" placeholder="Nomor telepon" name="nomortelepon" required="required">
+          <label for="phoneNumber">Nomor Telepon</label>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="form-label-group">
+          <input type="text" id="address" class="form-control" placeholder="Alamat" name="alamat" required="required">
+          <label for="address">Alamat</label>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="form-label-group">
+          <input type="email" id="inputEmail" class="form-control" placeholder="Email" name="email" required="required">
+          <label for="inputEmail">Email</label>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="form-label-group">
+          <input type="text" id="username" class="form-control" placeholder="Username" required="required" name="username">
+          <label for="username">Username</label>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="form-row">
+          <div class="col-md-6">
+            <div class="form-label-group">
+              <input type="password" id="inputPassword" class="form-control" placeholder="Kata sandi" name="katasandi" required="required">
+              <label for="inputPassword">Kata Sandi</label>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="form-label-group">
+              <input type="password" id="confirmPassword" class="form-control" placeholder="Konfirmasi kata sandi" required="required">
+              <label for="confirmPassword">Konfirmasi Kata Sandi</label>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <!-- <div class="form-group">
+      <!-- <div class="form-group">
       <div class="form-label-group">
         <select id="status"
                 class="form-control"
@@ -124,43 +89,41 @@
         <option value="user">3 User</option>
       </div>
     </div> -->
-    
-  <button type="submit"
-     class="btn btn-primary"
-     name="tambahuser"
-     href="login.php">
-     Tambah User
-  </button>
 
-  </form>
-</div>
+      <button type="submit" class="btn btn-primary" name="tambahuser" href="login.php">
+        Tambah User
+      </button>
 
-<?php
-$conn = mysqli_connect("localhost", "root", "", "naura_farm");
-if($conn === false){
+    </form>
+  </div>
+
+  <?php
+  $conn = mysqli_connect("localhost", "root", "", "naura_farm");
+  if ($conn === false) {
     die("Ono error iki lo: " . mysqli_connect_error());
-}
+  }
 
-$namalengkap = isset($_POST['namalengkap']) ? $_POST['namalengkap'] : null;
-$nomortelepon = isset($_POST['nomortelepon']) ? $_POST['nomortelepon'] : null;
-$alamat = isset($_POST['alamat']) ? $_POST['alamat'] : null;
-$email = isset($_POST['email']) ? $_POST['email'] : null;
-$username = isset($_G_POSTET['username']) ? $_POST['username'] : null;
-$katasandi = isset($_POST['katasandi']) ? $_POST['katasandi'] : null;
-$tambahuser = isset($_POST['tambahuseralamat']) ? $_POST['tambahuser'] : null;
+  $namalengkap = isset($_POST['namalengkap']) ? $_POST['namalengkap'] : null;
+  $nomortelepon = isset($_POST['nomortelepon']) ? $_POST['nomortelepon'] : null;
+  $alamat = isset($_POST['alamat']) ? $_POST['alamat'] : null;
+  $email = isset($_POST['email']) ? $_POST['email'] : null;
+  $username = isset($_POST['username']) ? $_POST['username'] : null;
+  $katasandi = isset($_POST['katasandi']) ? $_POST['katasandi'] : null;
+  $tambahuser = isset($_POST['tambahuseralamat']) ? $_POST['tambahuser'] : null;
 
-if(isset ($_POST['tambahuser'])) {
-    $sql = $conn ->query("insert into user (NAMA, NOMOR_TELEPON, ALAMAT, EMAIL, USERNAME, PASSWORD)values('$namalengkap', '$nomortelepon', '$alamat', '$email', '$username', '$katasandi')");
-    if($sql){
-        ?>
-        <script type="text/javascript">
-        alert ("Data Berhasil Disimpan");
-        window.location.href="?page=tabel-user";
-        </script>
-        <?php
+  if (isset($_POST['tambahuser'])) {
+    $sql = $conn->query("insert into user (NAMA, NOMOR_TELEPON, ALAMAT, EMAIL, USERNAME, PASSWORD)
+    values('$namalengkap', '$nomortelepon', '$alamat', '$email', '$username', '$katasandi')");
+    if ($sql) {
+      ?>
+      <script type="text/javascript">
+        alert("Data Berhasil Disimpan");
+        window.location.href = "?page=tabel-user";
+      </script>
+  <?php
     }
-}
-?>
+  }
+  ?>
 
   <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
