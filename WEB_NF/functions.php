23b0cfd7576937d8fb400761183d1edabb0bef3c
Dickyirqi <dickyirqi11@gmail.com>
2019-12-12 17:54:40 +0700

    Update functions.php

--- WEB_NF/functions.php
+++ WEB_NF/functions.php
@@ -13,4 +13,24 @@ function query($query)
         }
         return $rows;
 }
+
+
+//memasukkan keranjang
+
+function keranjang($krj){
+    global $conn;
+        $id_transaksi = htmlspecialchars($krj["id_transaksi"]);
+        $id_produk = htmlspecialchars($krj["id_produk"]);
+        $id_user = htmlspecialchars($krj["id_user"]);
+        $jumlah = htmlspecialchars($krj["jumlah"]);
+        $tanggal = htmlspecialchars($krj["tanggal"]);
+        $alamat = htmlspecialchars($krj["alamat"]);
+        $opsi = htmlspecialchars($krj["opsi"]);
+        $total = htmlspecialchars($krj["total"]);
+
+        $trs = mysqli_query($conn, "INSERT INTO keranjang VALUES ('$id_transaksi', '$id_produk', '$id_user', '$jumlah', '$tanggal', '$alamat', '$opsi', '$total')");
+
+
+        return $trs;
+}
 ?>
