bbf555552b0f14b0300f245f4eeb4e6676059905
Dickyirqi <dickyirqi11@gmail.com>
2019-12-12 15:48:43 +0700

    Update transaksi.php

--- WEB_NF/transaksi.php
+++ WEB_NF/transaksi.php
@@ -1,3 +1,9 @@
+<?php
+require 'functions.php';
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,6 +23,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap1.css" rel="stylesheet">
 
     <!-- Custom fonts -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
@@ -28,82 +35,81 @@
 
     <!-- Web Icon -->
     <link rel="shortcut icon" href="img/ic_nf.ico" />
+    <style>
+        body {
+            background-image: url(img/bg-masthead.JPG);
+        }
+    </style>
 </head>
-
-<body id="page-top" class="bg-dark">
-
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-        <div class="container-fluid" style="padding-left: 100px!important;
-    padding-right: 100px!important;">
-            <div class="logo">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top"> <img src="img/logo_nf.png" style="width: 60px;">
-                    Naura Farm
-                </a>
-            </div>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#profil">Profil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#produk">Produk</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#fasilitas">Fasilitas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link js-scroll-trigger" href="#kontak_kami">Kontak Kami</a>
-                    </li>
-                    <div class="dropdown">
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-user-circle fa-fw"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                                <!-- Session utk merubah nav akun login/register menjadi akun/keluar saat kondisi user sedang login -->
-                                <?php
-                                if (isset($_SESSION['login'])) {
-                                    ?>
-                                    <a class="dropdown-item" href="#" onclick="document.getElementById('id01').style.display='block'">Akun</a>
-                                    <a class="dropdown-item" href="keluar.php">Keluar</a>
-                                <?php
-                                } else {
-                                    ?>
-                                    <a class="dropdown-item" href="#" onclick="document.getElementById('id01').style.display='block'">Masuk</a>
-                                    <a class="dropdown-item" href="#" onclick="document.getElementById('id02').style.display='block'">Daftar</a>
-                                <?php } ?>
-                        </li>
-                </ul>
-            </div>
-            </ul>
-        </div>
-        </div>
-    </nav>
+
 
     <body>
-        <!-- Header -->
-        <header class="masthead">
-            <div class="container d-flex h-100 align-items-center">
-                <div class="mx-auto text-center">
-                   <div class="container">
-                       <h1>Produk</h1>
-                       <div class="row">
-                        <div class="col-md-3">
-                            <div class="thumbnail">
-                                <img src="img/buah_merah.jpg" alt="" height="100" width="100">
-                                <div class="caption">
-                                    <h3>Buah Naga Merah</h3>
-                                </div>                                
-                            </div>
-                        </div>
+
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div class="container-fluid" style="padding-left: 100px!important;
+    padding-right: 100px!important;">
+                    <div class="logo">
+                        <a class="navbar-brand js-scroll-trigger" href="#page-top"> <img src="img/logo_nf.png" style="width: 60px;">
+                            Naura Farm
+                        </a>
                     </div>
-                   </div>
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        Menu
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger" href="#profil">Profil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger" href="#produk">Produk</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger" href="#fasilitas">Fasilitas</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger" href="#kontak_kami">Kontak Kami</a>
+                            </li>
+                            <div class="dropdown">
+                                <li class="nav-item dropdown no-arrow">
+                                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fas fa-user-circle fa-fw"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                                        <!-- Session utk merubah nav akun login/register menjadi akun/keluar saat kondisi user sedang login -->
+                                        <?php
+                                        if (isset($_SESSION['login'])) {
+                                            ?>
+                                            <a class="dropdown-item" href="#" onclick="document.getElementById('id01').style.display='block'">Akun</a>
+                                            <a class="dropdown-item" href="keluar.php">Keluar</a>
+                                        <?php
+                                        } else {
+                                            ?>
+                                            <a class="dropdown-item" href="#" onclick="document.getElementById('id01').style.display='block'">Masuk</a>
+                                            <a class="dropdown-item" href="#" onclick="document.getElementById('id02').style.display='block'">Daftar</a>
+                                        <?php } ?>
+                                </li>
+                        </ul>
+                    </div>
+                    </ul>
                 </div>
-            </div>
+                </div>
+            </nav>
         </header>
+        <section>
+            <?php $ambil = $conn->query("SELECT * FROM produk"); ?>
+            <?php while ($perproduk = $ambil->fetch_assoc()) { ?>
+                <div class="card tambah" style="width: 18rem;">
+                    <img style="height: 190px; width: 265px;" class="card-img-top img" src="img/<?php echo $perproduk["FOTO_PRODUK"] ?>" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Rp. <?php echo $perproduk["HARGA_JUAL"];?></h5>
+                        <p class="card-text"><?php echo $perproduk["NAMA_PRODUK"];?></p>
+                        <a href="transaksiproduk.php?id=<?php echo $perproduk["ID_PRODUK"];?>" class="btn btn-primary">Beli</a>
+                    </div>
+                </div>
+            <?php } ?>
+        </section>
     </body>
