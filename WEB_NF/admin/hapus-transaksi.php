a5534184b10cf6eb761a2760766a149fced6f060
Dickyirqi <dickyirqi11@gmail.com>
2019-12-31 00:11:02 +0700

    Update hapus-transaksi.php

--- WEB_NF/admin/hapus-transaksi.php
+++ WEB_NF/admin/hapus-transaksi.php
@@ -1,8 +1,8 @@
 <?php
-require "koneksi.php";
+$conn = mysqli_connect("localhost","root","","naura_farm");
   $id = $_GET['id'];
   $conn->query("DELETE FROM transaksi WHERE ID_TRANSAKSI = '$id'");
-  echo "<script>alert('data berhasil dihapus'); window.location.href='http://127.0.0.1/REPO_WEB_NF/WEB_NF/admin/?page=tabel-pesanan'</script>";
+  echo "<script>alert('data berhasil dihapus'); window.location.href='/REPO_WEB_NF/WEB_NF/admin/?page=tabel-transaksi'</script>";
 ?>
 
 <!-- <script type="text/javascript">
