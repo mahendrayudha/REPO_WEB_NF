022da61ab4471b98174b08830b06b8be801851e9
iBOT03 <andrea.santana986@gmail.com>
2019-12-30 23:04:21 +0700

    nyoba ngapus detail jual pada saat ngapus transaksi gk bisa masih

--- WEB_NF/admin/hapus-transaksi.php
+++ WEB_NF/admin/hapus-transaksi.php
@@ -1,8 +1,14 @@
 <?php
 require "koneksi.php";
   $id = $_GET['id'];
-  $conn->query("DELETE FROM transaksi WHERE ID_TRANSAKSI = '$id'");
+  $hapus = $conn->query ("DELETE FROM transaksi WHERE ID_TRANSAKSI = '$id'");
   echo "<script>alert('data berhasil dihapus'); window.location.href='http://127.0.0.1/REPO_WEB_NF/WEB_NF/admin/?page=tabel-pesanan'</script>";
+  if ($conn->query($hapus) === TRUE) {
+    $h = mysqli_query($conn, "DELETE FROM detail_jual WHERE ID_TRANSAKSI = '$idtr'");
+  } else {
+    echo "Error: " . $simpan . "<br>" . $conn->error;
+  }
+
 ?>
 
 <!-- <script type="text/javascript">
